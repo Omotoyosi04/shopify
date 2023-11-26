045953357acfc0e9a746ad8f9db50fbc22a67508
Omotoyosi04 <109534966+Omotoyosi04@users.noreply.github.com>
2023-11-26 13:34:57 -0800

    script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const userInfo = document.querySelector('.userDropdown');
 const subPlan = document.querySelector('.subscriptionPlan')
 const cancelPlan = document.querySelector('.cancelPlan')
 const mainToggle = document.querySelector('.toggleArrows')
+const setupHeads = document.querySelectorAll(".setupHead");
+const setupTexts = document.querySelectorAll('.setupText')
 
 notificationBell.addEventListener('click', () => {
     if (alertMsg.style.display === 'none') {
@@ -41,3 +43,75 @@ cancelPlan.addEventListener('click', () => {
 mainToggle.addEventListener('click', () => {
     mainToggle.classList.toggle('active')
 })
+
+Array.from(setupHeads).forEach(setupHead => {
+    console.log('You are mad')
+    setupHeads.forEach(setupHead => {
+        setupHead.addEventListener('click', (event) => {
+        const clickSection = event.currentTarget
+        clickSection.classList.toggle('active')
+    })
+})
+})
+
+
+
+const progressNumber = document.querySelector('.setupProgress span');
+const checkCircles = document.querySelectorAll('.checkCircle');
+const progressBar = document.querySelector('.progressBar')
+
+let numberOfBlueCircles = 0;
+
+Array.from(checkCircles).forEach(function (circle) {
+    // Initialize the checked state as a data attribute
+    circle.dataset.checked = '0';
+
+    circle.addEventListener('click', (event) => {
+        const theCircle = event.currentTarget;
+
+        // Toggle the checked state using the data attribute
+        if (theCircle.dataset.checked === '0') {
+            theCircle.dataset.checked = '1';
+            console.log(theCircle.dataset.checked);
+            theCircle.style.backgroundImage = 'url(https://crushingit.tech/hackathon-assets/icon-checkmark-circle.svg)'
+            theCircle.style.backgroundPosition = 'center'
+            theCircle.style.border = 'none'
+            numberOfBlueCircles++; // Increment the count
+        } else {
+            theCircle.dataset.checked = '0';
+            console.log(theCircle.dataset.checked);
+            theCircle.style.backgroundImage = '';
+            theCircle.style.border = ''
+
+            numberOfBlueCircles--; // Decrement the count
+        }
+
+        console.log(`Number of blue circles: ${numberOfBlueCircles}`);
+    });
+
+    circle.addEventListener('click',() =>{
+        if(numberOfBlueCircles === 1){
+            progressNumber.innerHTML = `1/5 completed`
+            progressBar.style.width = `20%`
+        } else if(numberOfBlueCircles === 2){
+            progressNumber.innerHTML = `2/5 completed`
+            progressBar.style.width = `40%`
+        } else if(numberOfBlueCircles === 3){
+            progressNumber.innerHTML = `3/5 completed`
+            progressBar.style.width = `60%`
+
+        } else if(numberOfBlueCircles === 4){
+            progressNumber.innerHTML = `4/5 completed`
+            progressBar.style.width = `80%`
+
+        } else if(numberOfBlueCircles === 5){
+            progressNumber.innerHTML = `5/5 completed`
+            progressBar.style.width = `100%`
+
+        } else{
+            progressNumber.innerHTML = `0/5 completed`
+            progressBar.style.width = `0%`
+
+        }
+    })
+});
